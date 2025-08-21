589376257c8d5f636cb195497f13d5fa35eb29e7
designeer1 <babukalyan523@gmail.com>
2025-08-21 11:39:25 +0530

    Added gunicorn and fixed start command for Render

--- akash_pro/settings.py
+++ akash_pro/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-kslev@81$!7&)+(5#saakvj5i9_atx+m$2pwxb$+onuhg8u846
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["day-1-1.onrender.com", "localhost", "127.0.0.1"]
+
 
 
 # Application definition
